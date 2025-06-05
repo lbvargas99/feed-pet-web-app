d177210ecb66b05d43ffd5e3f50a7b61f8f4075d
Leonardo Borges <lbvargas99@gmail.com>
2025-06-04 21:56:50 -0300

    feat: added react hook form on signin form

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,11 +1,19 @@
+import { useForm } from "react-hook-form";
+
 function Signin() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   return (
     <>
       <div className="h-screen w-screen flex">
         <div className="bg-teal-900 w-1/2 h-auto flex items-center justify-center">
             <h2 className="text-4xl">Feed<strong className="text-white">Pet</strong></h2>
         </div>
-        <div className="w-1/2 h-screen"></div>
+        <div className="w-1/2 h-screen">
+          <form>
+            
+          </form>
+        </div>
       </div>
     </>
   );
